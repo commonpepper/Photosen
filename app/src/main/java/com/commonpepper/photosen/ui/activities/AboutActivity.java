e98bc0858e44d4e4b0761e38be4fe67eca621df1
commonpepper <46593780+commonpepper@users.noreply.github.com>
2019-03-01 11:24:56 +0300

    about activity improvement

--- app/src/main/java/com/commonpepper/photosen/ui/activities/AboutActivity.java
+++ app/src/main/java/com/commonpepper/photosen/ui/activities/AboutActivity.java
@@ -1,7 +1,14 @@
 package com.commonpepper.photosen.ui.activities;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.commonpepper.photosen.BuildConfig;
 import com.commonpepper.photosen.R;
 import com.google.android.material.navigation.NavigationView;
 
@@ -17,6 +24,7 @@ public class AboutActivity extends AbstractNavActivity {
         Toolbar toolbar = findViewById(R.id.about_toolbar);
         drawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        TextView versionView = findViewById(R.id.version_view);
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -26,11 +34,36 @@ public class AboutActivity extends AbstractNavActivity {
         navigationView.getMenu().findItem(R.id.drawer_about).setCheckable(true);
         navigationView.getMenu().findItem(R.id.drawer_about).setChecked(true);
         navigationView.setNavigationItemSelectedListener(this);
+
+        versionView.setText(BuildConfig.VERSION_NAME);
+
+        LinearLayout[] containers = new LinearLayout[]{
+                findViewById(R.id.layout_about_version),
+                findViewById(R.id.layout_about_github)
+        };
+        for (LinearLayout r : containers) {
+            r.setOnClickListener(clickListener);
+        }
+
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    private View.OnClickListener clickListener = (view -> {
+        switch (view.getId()) {
+            case R.id.layout_about_github:
+                openInBrowser("https://" + getResources().getString(R.string.github_link));
+                break;
+        }
+    });
+
+    private void openInBrowser(String url) {
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
     }
 }
