5f85c73a3947f0faaf54f8c825d8ae7e156de72d
commonpepper <46593780+commonpepper@users.noreply.github.com>
2019-12-04 16:49:20 +0300

    migration

--- app/src/main/java/com/commonpepper/photosen/model/PhotoDetails.kt
+++ app/src/main/java/com/commonpepper/photosen/model/PhotoDetails.kt
@@ -1,42 +1,11 @@
 package com.commonpepper.photosen.model
 
 class PhotoDetails {
-    /**
-     * photo : {"id":"33213028138","secret":"b119361ce4","server":"7840","farm":8,"dateuploaded":"1550111763","isfavorite":0,"license":0,"safety_level":0,"rotation":0,"owner":{"nsid":"110745934@N07","username":"nikunj.m.patel","realname":"","location":"","iconserver":"695","iconfarm":1,"path_alias":""},"title":{"_content":"Lesser Scaup"},"description":{"_content":""},"visibility":{"ispublic":1,"isfriend":0,"isfamily":0},"dates":{"posted":"1550111763","taken":"2019-02-13 21:34:06","takengranularity":0,"takenunknown":1,"lastupdate":"1550285861"},"views":"118206","editability":{"cancomment":1,"canaddmeta":0},"publiceditability":{"cancomment":1,"canaddmeta":0},"usage":{"candownload":0,"canblog":1,"canprint":0,"canshare":1},"comments":{"_content":74},"notes":{"note":[]},"people":{"haspeople":0},"tags":{"tag":[{"id":"110724604-33213028138-31942","author":"110745934@N07","authorname":"nikunj.m.patel","raw":"Waterfowl","_content":"waterfowl","machine_tag":0},{"id":"110724604-33213028138-241","author":"110745934@N07","authorname":"nikunj.m.patel","raw":"wild","_content":"wild","machine_tag":0},{"id":"110724604-33213028138-5833","author":"110745934@N07","authorname":"nikunj.m.patel","raw":"wildlife","_content":"wildlife","machine_tag":0},{"id":"110724604-33213028138-791","author":"110745934@N07","authorname":"nikunj.m.patel","raw":"nature","_content":"nature","machine_tag":0},{"id":"110724604-33213028138-1935","author":"110745934@N07","authorname":"nikunj.m.patel","raw":"Photography","_content":"photography","machine_tag":0},{"id":"110724604-33213028138-8010","author":"110745934@N07","authorname":"nikunj.m.patel","raw":"ducks","_content":"ducks","machine_tag":0},{"id":"110724604-33213028138-87701","author":"110745934@N07","authorname":"nikunj.m.patel","raw":"scaup","_content":"scaup","machine_tag":0},{"id":"110724604-33213028138-287417","author":"110745934@N07","authorname":"nikunj.m.patel","raw":"delmarva","_content":"delmarva","machine_tag":0},{"id":"110724604-33213028138-29622","author":"110745934@N07","authorname":"nikunj.m.patel","raw":"migration","_content":"migration","machine_tag":0},{"id":"110724604-33213028138-201","author":"110745934@N07","authorname":"nikunj.m.patel","raw":"winter","_content":"winter","machine_tag":0}]},"urls":{"url":[{"type":"photopage","_content":"https://www.flickr.com/photos/110745934@N07/33213028138/"}]},"media":"photo"}
-     * stat : ok
-     */
 
     var photo: PhotoBean? = null
     var stat: String? = null
 
     class PhotoBean {
-        /**
-         * id : 33213028138
-         * secret : b119361ce4
-         * server : 7840
-         * farm : 8
-         * dateuploaded : 1550111763
-         * isfavorite : 0
-         * license : 0
-         * safety_level : 0
-         * rotation : 0
-         * owner : {"nsid":"110745934@N07","username":"nikunj.m.patel","realname":"","location":"","iconserver":"695","iconfarm":1,"path_alias":""}
-         * title : {"_content":"Lesser Scaup"}
-         * description : {"_content":""}
-         * visibility : {"ispublic":1,"isfriend":0,"isfamily":0}
-         * dates : {"posted":"1550111763","taken":"2019-02-13 21:34:06","takengranularity":0,"takenunknown":1,"lastupdate":"1550285861"}
-         * views : 118206
-         * editability : {"cancomment":1,"canaddmeta":0}
-         * publiceditability : {"cancomment":1,"canaddmeta":0}
-         * usage : {"candownload":0,"canblog":1,"canprint":0,"canshare":1}
-         * comments : {"_content":74}
-         * notes : {"note":[]}
-         * people : {"haspeople":0}
-         * tags : {"tag":[{"id":"110724604-33213028138-31942","author":"110745934@N07","authorname":"nikunj.m.patel","raw":"Waterfowl","_content":"waterfowl","machine_tag":0},{"id":"110724604-33213028138-241","author":"110745934@N07","authorname":"nikunj.m.patel","raw":"wild","_content":"wild","machine_tag":0},{"id":"110724604-33213028138-5833","author":"110745934@N07","authorname":"nikunj.m.patel","raw":"wildlife","_content":"wildlife","machine_tag":0},{"id":"110724604-33213028138-791","author":"110745934@N07","authorname":"nikunj.m.patel","raw":"nature","_content":"nature","machine_tag":0},{"id":"110724604-33213028138-1935","author":"110745934@N07","authorname":"nikunj.m.patel","raw":"Photography","_content":"photography","machine_tag":0},{"id":"110724604-33213028138-8010","author":"110745934@N07","authorname":"nikunj.m.patel","raw":"ducks","_content":"ducks","machine_tag":0},{"id":"110724604-33213028138-87701","author":"110745934@N07","authorname":"nikunj.m.patel","raw":"scaup","_content":"scaup","machine_tag":0},{"id":"110724604-33213028138-287417","author":"110745934@N07","authorname":"nikunj.m.patel","raw":"delmarva","_content":"delmarva","machine_tag":0},{"id":"110724604-33213028138-29622","author":"110745934@N07","authorname":"nikunj.m.patel","raw":"migration","_content":"migration","machine_tag":0},{"id":"110724604-33213028138-201","author":"110745934@N07","authorname":"nikunj.m.patel","raw":"winter","_content":"winter","machine_tag":0}]}
-         * urls : {"url":[{"type":"photopage","_content":"https://www.flickr.com/photos/110745934@N07/33213028138/"}]}
-         * media : photo
-         */
-
         var id: String? = null
         var secret: String? = null
         var server: String? = null
@@ -63,15 +32,6 @@ class PhotoDetails {
         var media: String? = null
 
         class OwnerBean {
-            /**
-             * nsid : 110745934@N07
-             * username : nikunj.m.patel
-             * realname :
-             * location :
-             * iconserver : 695
-             * iconfarm : 1
-             * path_alias :
-             */
 
             var nsid: String? = null
             var username: String? = null
@@ -84,10 +44,6 @@ class PhotoDetails {
         }
 
         class TitleBean {
-            /**
-             * _content : Lesser Scaup
-             */
-
             private var _content: String? = null
 
             fun get_content(): String? {
@@ -100,9 +56,6 @@ class PhotoDetails {
         }
 
         class DescriptionBean {
-            /**
-             * _content :
-             */
 
             private var _content: String? = null
 
@@ -116,11 +69,6 @@ class PhotoDetails {
         }
 
         class VisibilityBean {
-            /**
-             * ispublic : 1
-             * isfriend : 0
-             * isfamily : 0
-             */
 
             var ispublic = 0
             var isfriend = 0
@@ -129,13 +77,6 @@ class PhotoDetails {
         }
 
         class DatesBean {
-            /**
-             * posted : 1550111763
-             * taken : 2019-02-13 21:34:06
-             * takengranularity : 0
-             * takenunknown : 1
-             * lastupdate : 1550285861
-             */
 
             var posted: String? = null
             var taken: String? = null
@@ -146,10 +87,6 @@ class PhotoDetails {
         }
 
         class EditabilityBean {
-            /**
-             * cancomment : 1
-             * canaddmeta : 0
-             */
 
             var cancomment = 0
             var canaddmeta = 0
@@ -157,10 +94,6 @@ class PhotoDetails {
         }
 
         class PubliceditabilityBean {
-            /**
-             * cancomment : 1
-             * canaddmeta : 0
-             */
 
             var cancomment = 0
             var canaddmeta = 0
@@ -168,12 +101,6 @@ class PhotoDetails {
         }
 
         class UsageBean {
-            /**
-             * candownload : 0
-             * canblog : 1
-             * canprint : 0
-             * canshare : 1
-             */
 
             var candownload = 0
             var canblog = 0
@@ -183,9 +110,6 @@ class PhotoDetails {
         }
 
         class CommentsBean {
-            /**
-             * _content : 74
-             */
 
             private var _content = 0
 
@@ -204,9 +128,6 @@ class PhotoDetails {
         }
 
         class PeopleBean {
-            /**
-             * haspeople : 0
-             */
 
             var haspeople = 0
 
@@ -216,14 +137,6 @@ class PhotoDetails {
             var tag: List<TagBean>? = null
 
             class TagBean {
-                /**
-                 * id : 110724604-33213028138-31942
-                 * author : 110745934@N07
-                 * authorname : nikunj.m.patel
-                 * raw : Waterfowl
-                 * _content : waterfowl
-                 * machine_tag : 0
-                 */
 
                 var id: String? = null
                 var author: String? = null
@@ -245,10 +158,6 @@ class PhotoDetails {
             var url: List<UrlBean>? = null
 
             class UrlBean {
-                /**
-                 * type : photopage
-                 * _content : https://www.flickr.com/photos/110745934@N07/33213028138/
-                 */
 
                 var type: String? = null
                 private var _content: String? = null
