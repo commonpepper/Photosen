7c0a6ac64ff8b8e507d8609d2066289c14974ba6
commonpepper <46593780+commonpepper@users.noreply.github.com>
2019-12-23 07:37:59 +0300

    database version up

--- app/src/main/java/com/commonpepper/photosen/database/PhotosenDatabase.kt
+++ app/src/main/java/com/commonpepper/photosen/database/PhotosenDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.commonpepper.photosen.model.Photo
 
-@Database(entities = [Photo::class], version = 1)
+@Database(entities = [Photo::class], version = 2)
 abstract class PhotosenDatabase : RoomDatabase() {
     abstract val historyDao: HistoryDao
 }
